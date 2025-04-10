b1204c66188fd13326cdc48153abcb8c2697d43e
Abhigyan003 <abhigyan.official88@gmail.com>
2025-04-10 22:50:15 +0530

    added hashed keys

--- include/PersistentTreap.hpp
+++ include/PersistentTreap.hpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #include "Values.hpp"
 #include "Nodes.hpp"
+#include <string>
+#include <type_traits>
 
 using namespace std;
 std :: mt19937 static rng(std :: chrono::steady_clock::now().time_since_epoch().count());
@@ -22,6 +24,49 @@ std::ostream& operator<<(std::ostream& os, const std::optional<T>& opt) {
     return os;
 }
 
+// 64-bit FNV-1a constants
+constexpr uint64_t FNV_OFFSET_BASIS = 14695981039346656037ULL;
+constexpr uint64_t FNV_PRIME = 1099511628211ULL;
+
+struct FNV1aHasher {
+    template<typename Key>
+    uint64_t operator()(const Key& key) const {
+        std::string str = to_string_flex(key);
+        return fnv1a(str);
+    }
+
+private:
+    uint64_t fnv1a(const std::string& str) const {
+        uint64_t hash = FNV_OFFSET_BASIS;
+        for (char c : str) {
+            hash ^= static_cast<uint64_t>(c);
+            hash *= FNV_PRIME;
+        }
+        return hash;
+    }
+
+    // Helpers to convert key to string
+    template<typename T>
+    std::enable_if_t<std::is_arithmetic_v<T>, std::string>
+    to_string_flex(const T& val) const {
+        return std::to_string(val);
+    }
+
+    std::string to_string_flex(const std::string& val) const {
+        return val;
+    }
+
+    std::string to_string_flex(const char* val) const {
+        return std::string(val);
+    }
+
+    std::string to_string_flex(char c) const {
+        return std::string(1, c);
+    }
+};
+
+static FNV1aHasher hasher;
+
 template<typename Key, typename Value>
 Nodes<Key, Value> nodes;
 
@@ -31,20 +76,21 @@ Values<Value> values;
 template<typename Key, typename Value>
 struct Node{
     Key key;
+    uint64_t hkey;
     int vID;
     int y;
     std :: pair<int, int>p;
-    Node() : y(rng()), p({0, 0}) {}
-    Node(Key k, Value v) : key(k), y(rng()), p({0, 0}) { vID = values<Value>.add(v); }
-    Node(int id) : key(nodes<Key, Value>[id].key), vID(nodes<Key, Value>[id].vID), y(nodes<Key, Value>[id].y), p(nodes<Key, Value>[id].p) {}
+    Node() : y(rng()), p({0, 0}) {} 
+    Node(Key k, Value v) : key(k), hkey(hasher(key)), y(rng()), p({0, 0}) { vID = values<Value>.add(v); }
+    Node(int id) : key(nodes<Key, Value>[id].key), hkey(nodes<Key, Value>[id].key), vID(nodes<Key, Value>[id].vID), y(nodes<Key, Value>[id].y), p(nodes<Key, Value>[id].p) {}
 
     friend ostream& operator<<(std::ostream& os, const Node<Key, Value> &node) {
-        os << node.key << " " << node.vID << " " << node.y << " " << node.p.first << " " << node.p.second;
+        os << node.key << " " << node.hkey << " " <<  node.vID << " " << node.y << " " << node.p.first << " " << node.p.second;
         return os;
     }
 
     friend istream& operator>>(std::istream& is, Node<Key, Value> &node) {
-        is >> node.key >> node.vID >> node.y >> node.p.first >> node.p.second;
+        is >> node.key >> node.hkey >> node.vID >> node.y >> node.p.first >> node.p.second;
         return is;
     }
 };
@@ -68,17 +114,17 @@ int merge(int T1, int T2){
 }
 
 template<typename Key, typename Value>
-pair<int, int> split(int T, Key k){
+pair<int, int> split(int T, const Key &k, const uint64_t &hk){
     if(!T) return{0, 0};
     int id = nodes<Key, Value>.add(T);
-    if(nodes<Key, Value>[T].key > k){
-        auto res = split<Key, Value>(nodes<Key, Value>[id].p.first, k);
+    if(nodes<Key, Value>[T].hkey > hk || (nodes<Key, Value>[T].hkey == hk && nodes<Key, Value>[T].key > k)){
+        auto res = split<Key, Value>(nodes<Key, Value>[id].p.first, k, hk);
         nodes<Key, Value>[id].p.first = res.second;
         return {res.first, id};
     }
     else
     {
-        auto res = split<Key, Value>(nodes<Key, Value>[id].p.second, k);
+        auto res = split<Key, Value>(nodes<Key, Value>[id].p.second, k, hk);
         nodes<Key, Value>[id].p.second = res.first;
         return {id, res.second};
     }
@@ -96,21 +142,20 @@ struct Treap{
     Treap() : root(0) {}
     Treap(int ROOT) : root(ROOT) {}
 
-    optional<Value> find(int T, const Key &key){
+    optional<Value> find(int T, const Key &key, const uint64_t &hkey){
         if(!T)  return nullopt;
-        // cerr << T << '\n';
-        if(nodes<Key, Value>[T].key == key)
+        if(nodes<Key, Value>[T].hkey == hkey && nodes<Key, Value>[T].key == key)
             return values<Value>[nodes<Key, Value>[T].vID];
-        if(nodes<Key, Value>[T].key > key){
-            return find(nodes<Key, Value>[T].p.first, key);
+        if(nodes<Key, Value>[T].hkey > hkey || (nodes<Key, Value>[T].hkey == hkey && nodes<Key, Value>[T].key > key)){
+            return find(nodes<Key, Value>[T].p.first, key, hkey);
         }
-        return find(nodes<Key, Value>[T].p.second, key);
+        return find(nodes<Key, Value>[T].p.second, key, hkey);
     }
 
-    optional<Key> find_lessThan(int T, const Key &key){
+    optional<Key> find_lessThan(int T, const Key &key, const uint64_t &hkey){
         if(!T)  return nullopt;
-        if(nodes<Key, Value>[T].key < key){
-            optional<Value>val = find_lessThan(nodes<Key, Value>[T].p.second, key);
+        if(nodes<Key, Value>[T].hkey < hkey || (nodes<Key, Value>[T].hkey == hkey && nodes<Key, Value>[T].key < key)){
+            optional<Value>val = find_lessThan(nodes<Key, Value>[T].p.second, key, hkey);
             if(val.has_value()){
                 return val;
             }
@@ -118,28 +163,29 @@ struct Treap{
         }
         else
         {
-            return find_lessThan(nodes<Key, Value>[T].p.first, key);
+            return find_lessThan(nodes<Key, Value>[T].p.first, key, hkey);
         }
     }
 
-    int insert(int T, Key key, Value value){
+    int insert(int T, const Key &key, const Value &value){
         optional<Value> v;
-        if((v = find(T, key)).has_value()){
+        uint64_t hkey = hasher(key);
+        if((v = find(T, key, hkey)).has_value()){
             if(v == value)
                 return T;
             cerr << "Value already present !\n";
             return T;
         }
-        auto Split = split<Key, Value>(T, key);        int id = nodes<Key, Value>.add(key, value);
+        auto Split = split<Key, Value>(T, key, hkey);        int id = nodes<Key, Value>.add(key, value);
         return merge<Key, Value>(Split.first, merge<Key, Value>(id, Split.second)); 
     }
 
-    int remove(int T, Key key){
-        if(!find(T, key).has_value())   return T;
-        optional<Key> lt = find_lessThan(T, key);
-        auto sp1 = split<Key, Value>(T, key);
+    int remove(int T, const Key &key, const uint64_t &hkey){
+        if(!find(T, key, hkey).has_value())   return T;
+        optional<Key> lt = find_lessThan(T, key, hkey);
+        auto sp1 = split<Key, Value>(T, key, hkey);
         if(lt.has_value()){
-            auto sp2 = split<Key, Value>(sp1.first, *lt);
+            auto sp2 = split<Key, Value>(sp1.first, *lt, hasher(*lt));
             return merge<Key, Value>(sp2.first, sp1.second);
         }
         else
@@ -148,20 +194,23 @@ struct Treap{
         }
     }
 
-    void insert(Key key, Value value){
+    void insert(const Key &key, const Value &value){
         root = insert(root, key, value);
     }
 
-    optional<Value> find(Key key){
-        return find(root, key);
+    optional<Value> find(const Key &key){
+        uint64_t hkey = hasher(key);
+        return find(root, key, hkey);
     }
 
-    void remove(Key key){
-        root = remove(root, key);
+    void remove(const Key &key){
+        uint64_t hkey = hasher(key);
+        root = remove(root, key, hkey);
     }
 
-    void edit(Key key, Value value){
-        root = insert(remove(root, key), key, value);
+    void edit(const Key &key, const Value &value){
+        uint64_t hkey = hasher(key);
+        root = insert(remove(root, key, hkey), key, value);
     }
 
     int size(int T){
@@ -196,6 +245,7 @@ struct Treap{
             is >> node.vID;
             node.vID -= 1;
             is >> node.key >> node.y >> node.p.first >> node.p.second;
+            node.hkey = hasher(node.key);
             Value v;
             is >> v;
             nodes<Key, Value>.add(node);
